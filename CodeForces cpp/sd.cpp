c46a8bc8fe2e5b21e32e96203c93932f45b560be
Kirtee <kirtee.praja@gmail.com>
2022-05-27 23:58:51 +0530

    677A - Vanya and Fence

---
+++ CodeForces cpp/sd.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string s;
+    int temp = 0;
+    cin >> s;
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] >= 33 && s[i] <= 126)
+        {
+            if (s[i] == 'H' || s[i] == 'Q' || s[i] == '9')
+                temp = 1;
+        }
+    }
+    if (temp != 0)
+        cout << "YES";
+    else
+        cout << "NO";
+    return 0;
+}
