4c1d7855bdd07332c5266846d6d17d649371517d
Kirtee <kirtee.praja@gmail.com>
2022-06-10 22:05:12 +0530

    Codeforces Cpp problems

---
+++ CodeForces cpp/1520A - Do Not Be Distracted!.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, flag = 1;
+        cin >> n;
+        string s;
+        cin >> s;
+        int a[26] = {0};
+        for (int i = 0; i < n; i++)
+        {
+            a[s[i] - 'A']++;
+            if (a[s[i] - 'A'] > 1 && s[i] != s[i - 1])
+            {
+                flag = 0;
+                break;
+            }
+        }
+        if (flag == 1)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+    return 0;
+}
