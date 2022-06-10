4ffe2d99880222e38c980ea79520b8dd1461e0e3
Kirtee <kirtee.praja@gmail.com>
2022-06-11 00:12:00 +0530

    Codeforces Cpp problems

---
+++ CodeForces cpp/1343A - Candies.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        double final = log(n + 1) / log(2);
+        for (int k = 2; k < final + 1; k++)
+        {
+            double x = (n * 1.0) / ((long long int)pow(2, k) - 1);
+            long long y = n / ((long long int)pow(2, k) - 1);
+            if (x == y && x > 0)
+            {
+                cout << y << endl;
+                break;
+            }
+        }
+    }
+    return 0;
+}
