2e94514c6eb25966dd3cb1fe8a2311ed172ce4f3
Kirtee <kirtee.praja@gmail.com>
2022-06-11 09:59:20 +0530

    Codeforces Cpp problems

---
+++ CodeForces cpp/1475A - Odd Divisor.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+using namespace std;
+#define int64_t as int
+void solve()
+{
+    long long int n;
+    cin >> n;
+    if (n % 2)
+    {
+        cout << "YES\n";
+        return;
+    }
+    while (n % 2 == 0)
+        n /= 2;
+    if (n > 1)
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
