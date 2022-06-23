93cd5e3b91c887d51b68a026aea9e54e61fb2ad4
Kirtee <kirtee.praja@gmail.com>
2022-06-23 17:02:48 +0530

    CodeForces Cpp problems

---
+++ LeetCode cpp/58. Length of Last Word.cpp
@@ -0,0 +1,13 @@
+class Solution
+{
+public:
+    int lengthOfLastWord(string s)
+    {
+        string word;
+        stringstream ss(s);
+        while (ss >> word)
+        {
+        }
+        return word.size();
+    }
+};
