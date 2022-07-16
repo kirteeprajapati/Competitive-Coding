c98c4addac45bb56c8c7008b3a4fd46920b60de1
kirteeprajapati <kirtee.praja@gmail.com>
2022-07-16 11:07:02 +0530

    CodeForces Cpp problems

---
+++ CodeForces cpp/1325A - EhAb AnD gCd.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a;
+        cin >> a;
+        cout << 1 << " " << a - 1 << endl;
+    }
+    return 0;
+}
