ef64f893fa5a556f05b7380b4ad09386fdf6a4d7
kirteeprajapati <kirtee.praja@gmail.com>
2022-07-22 12:46:00 +0530

    CodeForces Cpp problems

---
+++ LeetCode cpp/56. Merge Intervals.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    vector<vector<int>> merge(vector<vector<int>> &intervals)
+    {
+        vector<vector<int>> mergedInterval;
+        if (intervals.size() == 0)
+            return mergedInterval;
+        sort(intervals.begin(), intervals.end());
+        vector<int> tempInterval = intervals[0];
+        ;
+        for (auto it : intervals)
+        {
+            if (it[0] <= tempInterval[1])
+                tempInterval[1] = max(tempInterval[1], it[1]);
+            else
+            {
+                mergedInterval.push_back(tempInterval);
+                tempInterval = it;
+            }
+        }
+        mergedInterval.push_back(tempInterval);
+        return mergedInterval;
+    }
+};
