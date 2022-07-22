b99ebe35f1ae782d9838a1690bf6d1f908a68732
kirteeprajapati <kirtee.praja@gmail.com>
2022-07-22 16:41:56 +0530

    CodeForces Cpp problems

--- LeetCode cpp/287. Find the Duplicate Number.cpp
+++ LeetCode cpp/287. Find the Duplicate Number.cpp
@@ -18,3 +18,27 @@ public:
         return max_idx;
     }
 };
+
+
+//or
+
+class Solution
+{
+public:
+    int findDuplicate(vector<int> &nums)
+    {
+        int fast = nums[0], slow = nums[0];
+        do
+        {
+            fast = nums[nums[fast]];
+            slow = nums[slow];
+        } while (fast != slow);
+        fast = nums[0];
+        while (fast != slow)
+        {
+            fast = nums[fast];
+            slow = nums[slow];
+        }
+        return slow;
+    }
+};
